7b88acfeac350824f97878109d954c28cc26920a
Sri Harsha Hari <sriharshahari@yahoo.com>
2022-05-27 13:59:45 +0530

    rps logic done. need to loop it

--- script.js
+++ script.js
@@ -1,64 +1,89 @@
-function game(){
-    let playerScore=0;
-    let compScore=0;
-    for(i=1;i<=5;i++){
-        let choice=Math.floor(Math.random()*3);
-        let compChoice=['rock','paper','scissors'];
-        let compSelection=compChoice[choice];
-        let playerChoice=prompt("what is your choice?");
-        let playerSelection=playerChoice.toLowerCase();
-        console.log(compSelection);
-        function round(playerSelection,compSelection){
-            if(playerSelection===compSelection) {
-                return "tie round";
-            }
-            else if(playerSelection=='rock'){
-                if(compSelection=='scissors'){
-                    playerScore++;
-                    return "player wins round";
-                }
-                else {
-                    compScore++;
-                    return "player lose round";
-                }
-            }
-        
-            else if(playerSelection=='paper'){
-                if(compSelection=='rock'){
-                    playerScore++;
-                    return "player wins round";
-                }
-                else {
-                    compScore++;
-                    return "player lose round";
-                }
-            }
+let playerScore=0;
+let compScore=0;
 
-            else if(playerSelection=='scissors'){
-                if(compSelection=='paper'){
-                    playerScore++;
-                    return "player wins round";
-                }
-                else {
-                    compScore++;
-                    return "player lose round";
-                }
-            }
+
+const playerscore=document.getElementById('playerscore');
+const compscore=document.getElementById('compscore');
+const winner=document.getElementById('winner');
+const output=document.getElementById('output');
+const rockbtn=document.getElementById('rock');
+const paperbtn=document.getElementById('paper');
+const scissorsbtn=document.getElementById('scissors');
+//const compselection=document.getElementById('compselection');
+
+rockbtn.addEventListener('click',() => {
+    handleClick('rock');
+})
+paperbtn.addEventListener('click',() => {
+    handleClick('paper');
+})
+scissorsbtn.addEventListener('click',() => {
+    handleClick('scissors');
+})
+
+
+let choice=Math.floor(Math.random()*3);
+let compChoice=['rock','paper','scissors'];
+let compSelection=compChoice[choice];
+console.log(compSelection);
+
+
+function handleClick(playerSelection){
+    round(playerSelection,compSelection);
+}
+
+
+function round(playerSelection,compSelection){
+    if(playerSelection===compSelection) {
+        output.textContent= "tie round";
+    }
+    else if(playerSelection=='rock'){
+        if(compSelection=='scissors'){
+            playerScore++;
+            output.textContent="player wins round";
         }
+        else {
+            compScore++;
+            output.textContent="player lose round";
+        }
+    }
+        
+    else if(playerSelection=='paper'){
+        if(compSelection=='rock'){
+            playerScore++;
+            output.textContent="player wins round";
+        }
+        else {
+            compScore++;
+            output.textContent="player lose round";
+        }
+    }
 
-        console.log(round(playerSelection,compSelection));
-    }
-    console.log(playerScore);
-    console.log(compScore);
-    if(playerScore>compScore){
-        return "player wins game";
-    }
-    else if(playerScore==compScore){
-        return "Game is a tie";
-    }
-    else {
-        return "Computer wins the game";
+    else if(playerSelection=='scissors'){
+        if(compSelection=='paper'){
+            playerScore++;
+            output.textContent="player wins round";
+        }
+        else {
+            compScore++;
+            output.textContent="player lose round";
+        }
     }
 }
 
-console.log(game());
+
+
+function game(){
+   for(i=1;i<=5;i++){
+       
+    }
+    if(playerScore>compScore){
+        winner.textContent="player wins game";
+    }
+    else if(playerScore==compScore){
+        winner.textContent="Game is a tie";
+    }
+    else {
+        winner.textContent="Computer wins the game";
+    }
+}
